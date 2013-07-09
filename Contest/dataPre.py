6f8bcacc036a1fcd52e59c5c8da1c9edb1097130
llchdyz <llchdyz@gmail.com>
2013-07-08 22:00:54 -0400

    Add compare function and resource function

--- Contest/dataPre.py
+++ Contest/dataPre.py
@@ -4,21 +4,48 @@ import pandas as pd
 # read in csv and return header and content seperately
 def readCSV(filename):
     content = pd.read_csv(filename)
-    print content[:1]
+    print content
     return content
 
-def parseContent(content):
-    res = content['resource'] == id
-    d = content(content['resource'] == id)
-    data = content.__delitem__("resource")
-    return id, data
+def resource(idRes, content):
+    resource = content.RESOURCE
+    resArray = np.array(resource)
+    index = (resArray == idRes)
+    data = content[index]
+    #print data
+    access = data.ACTION
+    path = data.drop('ACTION', axis = 1)
+    #print path
+    return access, path
 
-d=data((data(:,2)==id),:);
-access=d(:,1);
-path=d(:,2:end);
-def resource(id, data):
-    pass
+def compare(singlePath, idRes, content):
+    arrSinglePath = np.asarray(singlePath)
+    access, path = resource(idRes,content);
+    pLen = len(path)
+    shapePath = path.shape
+    p = np.zeros(shapePath)
+    for i in range(pLen):
+        p[i] = np.where( np.array(path[i : i + 1]) == arrSinglePath, 1 , 0)
+    arrAccess = np.asarray(access)
 
-
-readCSV('train.csv')
+    arrAccess = np.transpose(arrAccess)
+    p = np.vstack((np.asarray(access), p.T))
+    # now p is transposed
+    p = np.delete(p, (1), axis = 0)
     
+    rlen= len(p)
+    X = []
+    for i in range(rlen):
+
+        s = arrAccess[p[i] == 1]
+        if s is None:
+            X.append(0.5)
+            continue
+        X.append(np.mean(s))
+    return X
+        
+        
+content = readCSV('train.csv')
+access, path = resource(0, content)
+print path[:1]
+X = compare(path[:1], 0, content)
